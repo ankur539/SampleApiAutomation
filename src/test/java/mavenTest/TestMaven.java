fcf145d848821b317ffd1cfd95e4b0b4f6fa90ad
ankur539 <ankur.srivastava1591@gmail.com>
2022-11-27 11:56:51 +0530

    Selenium code changes

--- src/test/java/mavenTest/TestMaven.java
+++ src/test/java/mavenTest/TestMaven.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
@@ -21,26 +22,27 @@ import io.restassured.specification.ResponseSpecification;
 
 public class TestMaven {
 	
-	//WebDriver driver;
+	WebDriver driver;
 	
 	@BeforeSuite
 	public void beforeSuite() {
 		RestAssured.baseURI = "https://cdn-api.co-vin.in/api";
-		//System.setProperty("webdriver.chrome.driver", "E://StudyMaterial//SampleApiAutomation//chromedriver");
-		
-		//driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
+		System.out.println(System.getProperty("user.dir"));
+		driver = new ChromeDriver();
 	}
 	
 	
-	/*@Test(enabled = true)
+	@Test(enabled = true)
 	public void openGoogle(Method m) {
 		System.out.println("--------------"+m.getName()+"--------------");	
 		
 		driver.get("https://www.google.co.in");
 		driver.manage().window().maximize();
+		System.out.println("Title of the page opened : "+driver.getTitle());
 		driver.quit();
 		System.out.println("Goggle is successfully opened and closed");
-	}*/
+	}
 	
 	@Test
 	public void stateDetails(Method m) {
